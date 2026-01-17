49a038448808b6807498a0298a8dea590629d6d4
Habibur Rhaman <devhabib2005@gmail.com>
2026-01-17 19:41:02 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
       <Hero />
       <Education />
-      <Experience />
+      {/* <Experience /> */}
       <Skills />
       <Projects />
       <Contact />
