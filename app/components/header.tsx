17fd32fe7bc42ac832fe36df05cba80ee0811d64
habiburRhaman05 <devhabib2005@gmail.com>
2026-04-07 08:41:03 +0600

    addedd new achievements nav likn

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -16,6 +16,7 @@ const navItems = [
 
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
+  { name: "Achievements ", href: "#achievements " },
   { name: "Contact", href: "#contact" },
 ];
 
